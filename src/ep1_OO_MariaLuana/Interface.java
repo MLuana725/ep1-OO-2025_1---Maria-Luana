a14dc21f4e92d7c04fcbea037d8fea7c209c58dd
MLuana725 <marialuana.sl962@gmail.com>
2025-05-01 18:12:57 -0300

    teste 3

--- src/ep1_OO_MariaLuana/Interface.java
+++ src/ep1_OO_MariaLuana/Interface.java
@@ -3,7 +3,7 @@ package ep1_OO_MariaLuana;
 public class Interface {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		System.out.println("Essa será onde a interface irá aparcer");
 
 	}
 
