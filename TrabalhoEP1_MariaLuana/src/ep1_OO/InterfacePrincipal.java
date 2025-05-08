cbc4edbf84e2bf0b0495d20d89f97c1e599018cc
MLuana725 <marialuana.sl962@gmail.com>
2025-05-08 15:44:39 -0300

    Delete TrabalhoEP1_MariaLuana directory

--- TrabalhoEP1_MariaLuana/src/ep1_OO/InterfacePrincipal.java
+++
@@ -1,9 +0,0 @@
-package ep1_OO;
-
-public class InterfacePrincipal {
-
-	public static void main(String[] args) {
-		System.out.println("Aqui é a interface principal");
-	}
-
-}
--- TrabalhoEP1_MariaLuana/src/module-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 
- */
-/**
- * 
- */
-module TrabalhoEP1_MariaLuana {
-}
