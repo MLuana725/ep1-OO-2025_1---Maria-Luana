146bc1163fafb6fce5b442b644ecf101a647b2ef
MLuana725 <marialuana.sl962@gmail.com>
2025-05-08 16:36:30 -0300

    teste final de commit

--- ep1_OO_TrabalhoML/src/InterfacePrincipal.java
+++ ep1_OO_TrabalhoML/src/InterfacePrincipal.java
@@ -1,5 +1,5 @@
 public class InterfacePrincipal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+        System.out.println("Interface de interação");
     }
 }
