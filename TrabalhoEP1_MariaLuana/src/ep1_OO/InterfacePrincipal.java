1ffeb7ccddf1ff4258df2bcb7479e243b0970d49
MLuana725 <marialuana.sl962@gmail.com>
2025-05-08 15:35:10 -0300

    Add files via upload

---
+++ TrabalhoEP1_MariaLuana/src/ep1_OO/InterfacePrincipal.java
@@ -0,0 +1,9 @@
+package ep1_OO;
+
+public class InterfacePrincipal {
+
+	public static void main(String[] args) {
+		System.out.println("Aqui é a interface principal");
+	}
+
+}
---
+++ TrabalhoEP1_MariaLuana/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module TrabalhoEP1_MariaLuana {
+}
