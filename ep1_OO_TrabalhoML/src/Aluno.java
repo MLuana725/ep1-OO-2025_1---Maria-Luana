e2be2b792fed98b0374f78c11bf64bd89b7889e8
MLuana725 <marialuana.sl962@gmail.com>
2025-05-13 16:21:19 -0300

    Criação de uma parcela do sistema focada no registro de aluno. Foi criada classe para armazenar dados e outra para colocar em uma lista.

--- ep1_OO_TrabalhoML/src/Aluno.java
+++ ep1_OO_TrabalhoML/src/Aluno.java
@@ -1,3 +1,29 @@
-public class Aluno {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Aluno {
+    private String nomeAluno;
+    private String matricula;
+    private String curso;
+
+    // Lista estática que armazena todos os dados de alunos
+    private static List<AlunoInfo> listaDeAlunos = new ArrayList<>();
+
+    public Aluno(String nomeAluno, String matricula, String curso) {
+        this.nomeAluno = nomeAluno;
+        this.matricula = matricula;
+        this.curso = curso;
+
+        salvarDados(); // Armazena os dados na lista estática
+    }
+
+    private void salvarDados() {
+        AlunoInfo info = new AlunoInfo(nomeAluno, matricula, curso);
+        listaDeAlunos.add(info);
+    }
+
+    // Método para acessar a lista de alunos cadastrados
+    public static List<AlunoInfo> getListaDeAlunos() {
+        return listaDeAlunos;
+    }
 }
---
+++ ep1_OO_TrabalhoML/src/AlunoInfo.java
@@ -0,0 +1,29 @@
+public class AlunoInfo {
+    private String nome;
+    private String matricula;
+    private String curso;
+
+    public AlunoInfo(String nome, String matricula, String curso) {
+        this.nome = nome;
+        this.matricula = matricula;
+        this.curso = curso;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    @Override
+    public String toString() {
+        return "Nome: " + nome + ", Matrícula: " + matricula + ", Curso: " + curso; //1?versão
+    }
+}
+
