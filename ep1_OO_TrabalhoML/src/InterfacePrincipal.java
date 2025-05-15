f17272817c397e6d56fa4129d1a2bff2c4adb929
MLuana725 <marialuana.sl962@gmail.com>
2025-05-15 16:36:05 -0300

    testando salvamento de dados dos alunos

--- ep1_OO_TrabalhoML/src/InterfacePrincipal.java
+++ ep1_OO_TrabalhoML/src/InterfacePrincipal.java
@@ -1,5 +1,14 @@
 public class InterfacePrincipal {
     public static void main(String[] args){
         System.out.println("Interface de interação");
+
+        //teste do registro
+
+        Aluno aluno1 = new Aluno("maria", "123456", "engenharia");
+        Aluno aluno2 = new Aluno("pedro", "565656565", "engenharia");
+
+
+        Aluno.dadosArquivo("alunos.txt");
+
     }
 }
