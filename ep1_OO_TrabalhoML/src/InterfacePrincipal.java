58f020015ada0ba55e80077fbf9e90dd6e207320
MLuana725 <marialuana.sl962@gmail.com>
2025-05-20 17:28:31 -0300

    Menu Aluno: adição do coidgo quase completo de cadastrar o aluno no sistema, faltando a categoria aluno especial.

--- ep1_OO_TrabalhoML/src/InterfacePrincipal.java
+++ ep1_OO_TrabalhoML/src/InterfacePrincipal.java
@@ -36,7 +36,10 @@ public class InterfacePrincipal {
             case 4: 
                 System.out.println("encerrando programa......");
                 break;
-              
+
+            default:
+              System.out.println("Opção Inválida");
+              break; 
            
             }
 
@@ -49,30 +52,64 @@ public class InterfacePrincipal {
     public static void menuModoAluno(){
       Scanner scanner = new Scanner(System.in);
       int opc = 0;
-      while (opc != 3) {
+      while (opc != 7) {
          System.out.println("==================================================================");
          System.out.println("                         Modo Aluno");
          System.out.println("==================================================================");
          System.out.println("Escolha o que deseja fazer:");
          System.out.println("1-Cadastrar Aluno");
-         System.out.println("2-?????");
-         System.out.println("3-Voltar ao menu principal");
+         System.out.println("2-Mostrar Lista de Alunos");
+         System.out.println("3-Matricular o Aluno em uma Turma");
+         System.out.println("4-Editar Aluno");
+         System.out.println("5-Mostrar Lista de Alunos");
+         System.out.println("7-Voltar ao menu principal");
          opc = scanner.nextInt();
          scanner.nextLine();
            switch (opc) {
             case 1:
-             //cadastro
+             System.out.println("Nome do Aluno:");
+             String nome = scanner.nextLine();
+
+             System.out.println("Matricula do Aluno:");
+             String matricula = scanner.nextLine();
+
+             if (Aluno.checarMatricula(matricula)) {
+                System.out.println("Aluno já cadastrado.");
+             }
+
+             System.out.println("Curso:");
+             String curso = scanner.nextLine();
+
+             System.out.println("É um aluno especial?");
+             String r = scanner.nextLine();
+ 
+             if (r.equalsIgnoreCase("Sim")) {
+                System.out.println("adicionando a categoria aluno especial");
+                
+             }else{
+                new Aluno(nome, matricula, curso);
+             }
+
                break;
 
             case 2:
-             //?????
+             //lista
                break;
 
             case 3:
-             //encerrar
+             //matricula
               break;
+            
+            case 4:
+             //Editar aluno
+             break;
+              
+            case 7:
+             //encerrar
 
-           
+            default:
+              System.out.println("Opção Inválida");
+              break;
             }
 
         }
